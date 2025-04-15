593eb3862d7c1172511ed89121e9a8bfb9143227
Hidde Huitema <hhuitema@int6.local.snellius.surf.nl>
2025-04-15 11:49:28 +0200

    add bashfile for GRN training

--- train_grn.sh
+++ train_grn.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=9                                                              # Specify the number of CPUs/task
 #SBATCH --gpus=1                                                                        # Specify the number of GPUs to use
 #SBATCH --partition=gpu_mig                                                                 # Specify the node partition (see slides Cris)
-#SBATCH --time=01:00:00                                                                 # Specify the maximum time the job can run
+#SBATCH --time=00:10:00                                                                 # Specify the maximum time the job can run
 #SBATCH --output=/gpfs/home6/hhuitema/github_repos/SurgeSplam/slurm_outputs/%j.out      # Define output folder for slurm output files
 
 
@@ -20,10 +20,10 @@ export WANDB_CACHE_DIR=/gpfs/home6/hhuitema/github_repos/SurgeSplam/wandb
 export WANDB_START_METHOD="thread"
 wandb login
 
-srun apptainer exec --nv --bind /scratch-shared/hhuitema:/scratch-shared/hhuitema /home/hhuitema/docker/endogslam_v2.sif            torchrun --nnodes 1 --nproc_per_node 1 train_GRN.py \
+srun apptainer exec --nv /home/hhuitema/docker/endogslam_v2.sif                 torchrun --nnodes 1 --nproc_per_node 1 train_GRN.py \
                                                                                            --data_path  /gpfs/work5/0/tesr0602/Datasets/SurgeNet \
                                                                                            --depth_path /scratch-shared/hhuitema/depths/Unfrozen_SurgeNet_Encoder \
-                                                                                           --batch_size_per_gpu 48\
+                                                                                           --batch_size_per_gpu 1\
                                                                                            --num_workers 9\
                                                                                            --epochs 4 \
                                                                                            --output_dir /gpfs/home6/hhuitema/github_repos/SurgeSplam/logs/GRN_1 \
