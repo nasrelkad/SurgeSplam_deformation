116abe29f6aa86ae395506104d2b8016e1ba57b4
Hidde Huitema <hhuitema@int4.local.snellius.surf.nl>
2025-04-17 11:42:55 +0200

    minor config updates

--- create_vid.sh
+++ create_vid.sh
@@ -1,4 +1,4 @@
 cd /gpfs/home6/hhuitema/github_repos/SurgeSplam
 
 
-srun apptainer exec --nv /home/hhuitema/docker/endogslam_v2.sif                  ffmpeg -framerate 24 -i scripts/plots/tracking/rgb/1/%01d.png output.mp4
+srun apptainer exec --nv /home/hhuitema/docker/endogslam_v2.sif                  ffmpeg -framerate 24 -i scripts/plots/tracking/rgb/%01d/24.png output.mp4
