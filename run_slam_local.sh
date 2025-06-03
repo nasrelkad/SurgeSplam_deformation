2f03433a44a07121fea929aa6c081cd113fe4bb2
HiddeHuitema <h.r.huitema@student.tue.nl>
2025-06-03 09:29:08 +0200

    minor experiment updates

--- run_slam_local.sh
+++ run_slam_local.sh
@@ -4,13 +4,15 @@
 
 # python3  scripts/main_SurgeSplat.py configs/endonerf/endonerf_pulling_deform_simple.py 
 #
-# python3  scripts/main_SurgeSplat.py configs/endonerf/endonerf_deform_simple.py 
+# /python3  scripts/main_SurgeSplat.py configs/endonerf/endonerf_deform_simple.py 
 
-python3  scripts/main_SurgeSplat.py configs/StereoMis/stereomis.py
+python3  scripts/main_SurgeSplat.py configs/StereoMis/stereomis_seq_5.py
 
 # python3  scripts/main_SurgeSplat.py configs/RARP/rarp_1.py
 
 # python3  scripts/main_SurgeSplat.py configs/hamlyn/hamlyn_scene_01.py
+
+# python3  scripts/main_SurgeSplat.py configs/scared/scared_long.py
 
 # for file in /media/thesis_ssd/code/EndoGSLAM/EndoGSLAM/configs/Experiments_to_run/*
 # do 
