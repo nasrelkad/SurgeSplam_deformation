3072670df4692321e1b7bc992701e807c281adfd
Hidde Huitema <hhuitema@int6.local.snellius.surf.nl>
2025-06-03 09:31:12 +0200

    start working on visualizer Snellius

--- run_slam.sh
+++ run_slam.sh
@@ -4,5 +4,5 @@
 cd /gpfs/home6/hhuitema/github_repos/SurgeSplam
 
 
-srun apptainer exec --nv /home/hhuitema/docker/endogslam_v2.sif                  python3  scripts/main_SurgeSplat.py configs/RARP/rarp_1.py
+srun apptainer exec --nv /home/hhuitema/docker/endogslam_v2.sif                  python3  scripts/main_SurgeSplat.py configs/hamlyn/hamlyn_scene_01.py
 
